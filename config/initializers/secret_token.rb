a0635fbfeb7cb7d1a3aa9bc8ed84e7808880cad9
mdcougar <mdcougar@gmail.com>
2013-10-15 00:19:44 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Famdu::Application.config.secret_key_base = '232c3c1a874a33a9726c1eac420223a74f7a5ff8bffbb30e6d7070751788db32483293afb058261bc7c7dde41af79c30cd7d1f976f41a5306269a2cfac2f5d42'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Famdu::Application.config.session_store :cookie_store, key: '_famdu_session'
